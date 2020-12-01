623b1d3796de3944ba7fb2bf83da7abb47616d82
bhattaraimadhu56 <bhattaraimadhu56@gmail.com>
2020-12-01 18:50:02 +1300

    Make progressBuid separate class and use it in login and register page , use TextInputAction.next to go to next inputline

--- lib/screens/MyApp.dart
+++ lib/screens/MyApp.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_database/firebase_database.dart';
+// import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatefulWidget {
